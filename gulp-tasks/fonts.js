b5bf32fe1ef117c74a9c8d295c508e8abdf25cb0
Nils Binder <nils.binder@icloud.com>
2022-10-19 08:56:42 +0200

    Initial commit

---
+++ gulp-tasks/fonts.js
@@ -0,0 +1,19 @@
+const GetGoogleFonts = require("get-google-fonts");
+
+const fonts = async () => {
+  // Setup of the library instance by setting where we want
+  // the output to go. CSS is relative to output font directory
+  const instance = new GetGoogleFonts({
+    outputDir: "./dist/fonts",
+    cssFile: "./fonts.css",
+  });
+
+  // Grabs fonts and CSS from google and puts in the dist folder
+  const result = await instance.download(
+    "https://fonts.googleapis.com/css2?family=Work+Sans:wght@500;800&display=swap"
+  );
+
+  return result;
+};
+
+module.exports = fonts;
---
+++ src/shortcodes/icon.js
@@ -0,0 +1,13 @@
+// pass in collections.all | eleventyNavigation, (current) page, and maximum depth level
+module.exports = (icon) => {
+  let style = "";
+  let srText = "aria-hidden='true' focusable='false'";
+
+  if (icon.width) {
+    style += "width:" + icon.width + "; height:" + icon.width + ";";
+  }
+  if (icon.alt) {
+    srText = "aria-label='" + icon.alt + "'";
+  }
+  return `<svg style="${style}" class="icon" ${srText}><use xlink:href="/icons/icons.svg#${icon.icon}"/></svg>`;
+};

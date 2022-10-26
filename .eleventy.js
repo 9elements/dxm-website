de1127df477da90e2c529442602c3818a7e2ab15
KaiUweHella <kai.hella27@gmail.com>
2022-10-26 10:37:13 +0200

    initial work

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ const isProduction = process.env.NODE_ENV === "production";
 module.exports = (config) => {
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/images/");
+  config.addPassthroughCopy("./src/js/");
 
   // Add filters
   config.addFilter("readableDate", readableDate);

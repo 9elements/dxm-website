b08cbeb8735bc52e4f39439b96aa194e93c3d084
Milena Friedhoff <milena.friedhoff@gmail.com>
2022-11-29 11:31:20 +0100

    size logo grid

--- ctfl/pageMedia.js
+++ ctfl/pageMedia.js
@@ -4,6 +4,7 @@ module.exports = async () => {
   const pageMedia = await client
     .getEntries({
       content_type: "pageMedia",
+      include: 5,
     })
     .then(function (response) {
       const items = response.items.map(function (item) {

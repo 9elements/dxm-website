c1d21123f82159790975e8b545be8564a80ad704
KaiUweHella <kai.hella27@gmail.com>
2023-01-24 14:38:58 +0100

    edit order of publikation

--- ctfl/publikation.js
+++ ctfl/publikation.js
@@ -1,10 +1,11 @@
 // import the client
-const client = require("../src/utils/contentfulClient.js");
+const client = require('../src/utils/contentfulClient.js');
 
 module.exports = async () => {
   const publikation = await client
     .getEntries({
-      content_type: "publikation",
+      content_type: 'publikation',
+      order: 'fields.releaseYear',
     })
     .then(function (response) {
       const items = response.items.map(function (item) {

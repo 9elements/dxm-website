61164ee1b9ab1a1e1462c2777c3480c7564867f1
pumuk <103521343+KaiUweHella@users.noreply.github.com>
2023-03-17 12:49:59 +0100

    start with change the svg plugin

--- src/shortcodes/icon.js
+++ src/shortcodes/icon.js
@@ -1,13 +1,17 @@
 // pass in collections.all | eleventyNavigation, (current) page, and maximum depth level
 module.exports = (icon) => {
-  let style = "";
+  let style = '';
   let srText = "aria-hidden='true' focusable='false'";
+  let iconClass = 'icon';
 
   if (icon.width) {
-    style += "width:" + icon.width + "; height:" + icon.width + ";";
+    style += 'width:' + icon.width + '; height:' + icon.width + ';';
   }
   if (icon.alt) {
     srText = "aria-label='" + icon.alt + "'";
   }
-  return `<svg style="${style}" class="icon" ${srText}><use xlink:href="/icons/icons.svg#${icon.icon}"/></svg>`;
+  if (icon.class) {
+    iconClass = icon.class;
+  }
+  return `<svg style="${style}" class="${iconClass}" ${srText}><use xlink:href="/icons/icons.svg#svg-${icon.icon}"/></svg>`;
 };
